d361ac9dd0715dd754632e446ec4469f1b8c440b
whoastonic <kingstonicthe1@gmail.com>
2021-10-21 03:59:09 -0400

    adding .npmignore

--- packages/commons/lib/file-manager/utils.ts
+++
@@ -1,9 +0,0 @@
-export class FileError extends Error {
-	constructor (message?: string) {
-		super()
-
-		this.name = 'File Error'
-		this.message = message
-		|| 'An unhandled error has occurred.'
-	}
-}
