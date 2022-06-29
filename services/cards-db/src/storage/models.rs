b2401a965680f7cd90a53aa7b8ae01a92b054af7
whoastonic <kingstonicthe1@gmail.com>
2022-06-29 22:13:25 +0100

    base service logic, down.

--- services/cards-db/src/storage/models.rs
+++ services/cards-db/src/storage/models.rs
@@ -13,7 +13,7 @@ pub struct EntryCard {
 	pub _lval: Option<i32>,
 	pub _scale: Option<i32>,
 	pub _markers: Option<String>,
-	pub _has_effect: Option<String>
+	pub _has_effect: Option<String>,
 }
 
 #[derive(Debug, Insertable, Queryable, Serialize, Deserialize, Clone)]
@@ -24,7 +24,7 @@ pub struct EntryCardImg {
 	pub card_img_small: String,
 }
 
-#[derive(Debug, Insertable, Queryable, Deserialize, Clone)]
+#[derive(Debug, Insertable, Serialize, Queryable, Deserialize, Clone)]
 #[table_name = "entry_card_set"]
 pub struct EntryCardSet {
 	pub card_name: String,
@@ -33,16 +33,16 @@ pub struct EntryCardSet {
 	pub set_market: String,
 }
 
-#[derive(Debug, Insertable, Queryable, Deserialize, Clone)]
-#[table_name = "entry_card_price"]
-pub struct EntryCardPrice {
-	pub card_name: String,
-	pub market_name: String,
-	pub market_uri: String,
-	pub market_price: String,
-}
+// #[derive(Debug, Insertable, Queryable, Deserialize, Clone)]
+// #[table_name = "entry_card_price"]
+// pub struct EntryCardPrice {
+// 	pub card_name: String,
+// 	pub market_name: String,
+// 	pub market_uri: String,
+// 	pub market_price: String,
+// }
 
-#[derive(Debug, Insertable, Queryable, Deserialize, Clone)]
+#[derive(Debug, Insertable, Serialize, Queryable, Deserialize, Clone)]
 #[table_name = "entry_card_format"]
 pub struct EntryCardFormat {
 	pub card_name: String,
@@ -50,5 +50,5 @@ pub struct EntryCardFormat {
 	pub _ocg_limit: i32,
 	pub _goat_limit: i32,
 	pub _tcg_release: String,
-	pub _ocg_release: String
+	pub _ocg_release: String,
 }
--- services/cards-db/src/storage/schema.rs
+++ services/cards-db/src/storage/schema.rs
@@ -1,54 +1,54 @@
 table! {
-    entry_card (name) {
-        name -> Varchar,
-        card_desc -> Varchar,
-        card_race -> Varchar,
-        card_type -> Varchar,
-        _atk -> Nullable<Int4>,
-        _def -> Nullable<Int4>,
-        _lvl -> Nullable<Int4>,
-        _lval -> Nullable<Int4>,
-        _scale -> Nullable<Int4>,
-        _markers -> Nullable<Varchar>,
-        _has_effect -> Nullable<Bpchar>,
-    }
+		entry_card (name) {
+				name -> Varchar,
+				card_desc -> Varchar,
+				card_race -> Varchar,
+				card_type -> Varchar,
+				_atk -> Nullable<Int4>,
+				_def -> Nullable<Int4>,
+				_lvl -> Nullable<Int4>,
+				_lval -> Nullable<Int4>,
+				_scale -> Nullable<Int4>,
+				_markers -> Nullable<Varchar>,
+				_has_effect -> Nullable<Bpchar>,
+		}
 }
 
 table! {
-    entry_card_format (card_name) {
-        card_name -> Varchar,
-        _tcg_limit -> Int4,
-        _ocg_limit -> Int4,
-        _goat_limit -> Int4,
-        _tcg_release -> Varchar,
-        _ocg_release -> Varchar,
-    }
+		entry_card_format (card_name) {
+				card_name -> Varchar,
+				_tcg_limit -> Int4,
+				_ocg_limit -> Int4,
+				_goat_limit -> Int4,
+				_tcg_release -> Varchar,
+				_ocg_release -> Varchar,
+		}
 }
 
 table! {
-    entry_card_img (card_name) {
-        card_name -> Varchar,
-        card_img -> Varchar,
-        card_img_small -> Varchar,
-    }
+		entry_card_img (card_name) {
+				card_name -> Varchar,
+				card_img -> Varchar,
+				card_img_small -> Varchar,
+		}
 }
 
 table! {
-    entry_card_price (card_name) {
-        card_name -> Varchar,
-        market_name -> Varchar,
-        market_uri -> Varchar,
-        market_price -> Varchar,
-    }
+		entry_card_price (card_name) {
+				card_name -> Varchar,
+				market_name -> Varchar,
+				market_uri -> Varchar,
+				market_price -> Varchar,
+		}
 }
 
 table! {
-    entry_card_set (card_name) {
-        card_name -> Varchar,
-        set_name -> Varchar,
-        set_release -> Varchar,
-        set_market -> Varchar,
-    }
+		entry_card_set (card_name) {
+				card_name -> Varchar,
+				set_name -> Varchar,
+				set_release -> Varchar,
+				set_market -> Varchar,
+		}
 }
 
 joinable!(entry_card_format -> entry_card (card_name));
@@ -57,9 +57,9 @@ joinable!(entry_card_price -> entry_card (card_name));
 joinable!(entry_card_set -> entry_card (card_name));
 
 allow_tables_to_appear_in_same_query!(
-    entry_card,
-    entry_card_format,
-    entry_card_img,
-    entry_card_price,
-    entry_card_set,
+	entry_card,
+	entry_card_format,
+	entry_card_img,
+	entry_card_price,
+	entry_card_set,
 );
